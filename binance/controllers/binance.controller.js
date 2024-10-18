66c1dd83a21f427d3fc0c0bc3c38ffa04d159536
SasiKumar Sivaraman <sasi@MacBookPro.carolina.rr.com>
2024-10-18 19:07:33 -0400

    revisted structure and context path

--- binance/controllers/binance.controller.js
+++ binance/controllers/binance.controller.js
@@ -1,34 +1,75 @@
 const axios = require('axios');
 const crypto = require('crypto');
-const { success, error, validation } = require("../../../../../helpers/responseApi");
+const os = require('os');
+const { success, error, validation } = require("../helpers/responseApi");
+
+exports.getHello = async (req, res) => {
+    console.log('===> Inside getHello() : Welocme to 0xConnectors');
+    try {
+        res.send({
+            statusCode: res.statusCode,
+            statusMessage: 'success',
+            message: 'Successfully connected to hello'
+        });
+    } catch (error) {
+        res.send({
+            statusCode: res.statusCode,
+            statusMessage: 'error',
+            message: `Error connecting hello', ${error.response ? error.response.data : error.message}`,
+        });
+        console.error('Error connecting hello:', error.response ? error.response.data : error.message);
+    }
+}
+
+
+
+function getIPAddress() {
+  const interfaces = os.networkInterfaces();
+  for (const name of Object.keys(interfaces)) {
+    for (const iface of interfaces[name]) {
+      if (iface.family === 'IPv4' && !iface.internal) {
+        return iface.address;
+      }
+    }
+  }
+}
+
 
 exports.getTradeHistory = async (req, res) => {
-
-    const apiKey = req.get('apiKey');
-    const secretKey = req.get('secretKey');
-    const targetEndpointUrl = req.get('targetEndpointUrl');
-
-    const {
-        numberOfRows,
-        tradeSymbol
-    } = req.body;
-
-    const timestamp = Date.now(); // Get current timestamp
-    const queryString = `symbol=${tradeSymbol}&timestamp=${timestamp}&limit=${numberOfRows}`;
-
-    // Sign the request
-    const signature = crypto.createHmac('sha256', secretKey).update(queryString).digest('hex');
-
-    console.log(signature, 'signature');
-
-    // Set headers
-    const headers = {
-        'X-MBX-APIKEY': apiKey,
-    };
-
-    console.log(headers, 'headers');
-
     try {
+        console.log('===> Inside getTradeHistory()');
+
+        let ipaddress = getIPAddress();
+        console.log('===> Server IP Address:', ipaddress);
+
+        const apiKey = 'oS1UaFODwH7tGrpYRvsX1BD3ETjDZYcGD1lTUp3u3dtMlbIOAnIDsow5MKpF7uDQ'; //req.get('apiKey');
+        const secretKey = 'vTx3T7BGkFPgJrwxTVLLxMQOLYCYWah92jWnwdL54HUuaTBEKcsctlNiAJiE8h3O'; //req.get('secretKey');
+        const targetEndpointUrl = "https://testnet.binance.vision/api/v3/myTrades"; //req.get('targetEndpointUrl');
+
+        // const {
+        //     numberOfRows,
+        //     tradeSymbol
+        // } = req.body;
+
+        const tradeSymbol = "LINKBTC";
+        const numberOfRows = 10;
+
+        const timestamp = Date.now(); // Get current timestamp
+        const queryString = `symbol=${tradeSymbol}&timestamp=${timestamp}&limit=${numberOfRows}`;
+
+        // Sign the request
+        const signature = crypto.createHmac('sha256', secretKey).update(queryString).digest('hex');
+
+        //console.log(signature, 'signature');
+
+        // Set headers
+        const headers = {
+            'X-MBX-APIKEY': apiKey,
+        };
+
+        //console.log(headers, 'headers');
+
+
         const response = await axios.get(targetEndpointUrl, {
             headers: headers,
             params: {
@@ -81,8 +122,8 @@ exports.getTradeHistory = async (req, res) => {
         console.log(averageTrades);
 
         const tradeHistoryData = {
-            code : 'GTTRDHTY1001',
-            ipaddress : 'get-aws-server-instance-ipaddress',
+            code: 'GTTRDHTY1001',
+            ipaddress: ipaddress,
             tradeHistory: averageTrades
         }
 
