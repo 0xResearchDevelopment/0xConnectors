2684c8cffb853826986d9585dc07876a544707b6
SasiKumar Sivaraman <sasi@MacBookPro.carolina.rr.com>
2024-10-25 18:41:09 -0400

    change in /balance path

--- binance/routes/binance.routes.js
+++ binance/routes/binance.routes.js
@@ -6,6 +6,6 @@ const binanceController = require("../controllers/binance.controller.js");
 // Routes
 router.post("/tradehistory", binanceController.getTradeHistory);
 router.get("/hello", binanceController.getHello);
-router.post("/getBalance", binanceController.getBalance); //FIXME: change from /getBalance to /balance
+router.post("/balance", binanceController.getBalance); //FIXME: change from /getBalance to /balance
 
 module.exports = router;
