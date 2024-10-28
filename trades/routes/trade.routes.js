e739e65ea525ee1e97dd36aab668831eff8013c4
Sadish <sadish003@gmail.com>
2024-10-29 01:38:38 +0530

    Added signal-input-direct api

--- trades/routes/trade.routes.js
+++ trades/routes/trade.routes.js
@@ -5,5 +5,7 @@ const tradesController = require("../controllers/trade.controller.js");
 
 // Routes
 router.post("/signalinput", tradesController.signalInput);
+router.get("/orderdepth", tradesController.getOrderDepth);
+router.post("/signalinputdirect", tradesController.signalInputDirect);
 
 module.exports = router;
