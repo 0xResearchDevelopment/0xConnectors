a0118a2fe8149cffe58a3b334f4f4062065a1f2d
Sadish <sadish003@gmail.com>
2024-12-02 23:33:41 +0530

    Added Market Order API and splitted limit and market flow seperately

--- trades/controllers/trade.controller.js
+++ trades/controllers/trade.controller.js
@@ -7,78 +7,139 @@ exports.signalInput = async (req, res) => {
     try {
         console.log('===> Inside signalInput()');
 
-        // Extracting values from request body
-        const platform = req.body.platform;
-        const clientIdCode = req.body.clientIdCode;
-        const clientEmail = req.body.clientEmail;
-        const tradeTimeframe = req.body.tradeTimeframe; 
-        const baseCurrencyCode = req.body.baseCurrencyCode;
-        const tokenCurrenyCode = req.body.tokenCurrenyCode; 
-        const tradeSymbol = req.body.tradeSymbol;
-        const allowSimulationFlag = req.body.allowSimulationFlag; 
-        const tradeAction = req.body.tradeAction;  
+        const tradeSignals = await tradeService.getTradeSignal();
 
-        console.log('===> Trade Symbol:', tradeSymbol);
-        console.log('===> Platform:', platform);
-        console.log('===> Trade Action:', tradeAction);
-        console.log('===> Trade Timeframe:', tradeTimeframe);
-
-        const signals = await tradeService.getSignalInput(tradeAction, tradeSymbol, platform, tradeTimeframe)
-
-        console.log('===> Signal Input Response:', signals);
-
-        if(!signals) {
+        if(!tradeSignals) {
             res.status(500).json({
                 statusCode: res.statusCode,
                 statusMessage: 'error',
-                message: `Error fetching signal input data.`,
+                message: `Error fetching trade signal data.`,
             });
         }
 
-        //Executing trades based on the received signal response
-        for (let signal of signals) {
+        console.log('===> Trade Signal Response:', tradeSignals);
 
-            const executeTradeRes = await tradeService.executeTrade(signal.API_KEY, signal.API_SECRET, signal.ENDPOINT_URL, signal.BOT_SYMBOL, signal.TRADE_QUANTITY, signal.TRADE_ACTION)
+        for (let tradeSignal of tradeSignals) {
+
+            console.log('===> tradeSignal.TRADE_ACTION:', tradeSignal.TRADE_ACTION);
+            console.log('===> tradeSignal.TRADE_SYMBOL:', tradeSignal.TRADE_SYMBOL);
+            console.log('===> tradeSignal.PLATFORM:', tradeSignal.PLATFORM);
+            console.log('===> tradeSignal.TRADE_TIMEFRAME:', tradeSignal.TRADE_TIMEFRAME);
+
+            const signals = await tradeService.getSignalInput(tradeSignal.TRADE_ACTION, tradeSignal.TRADE_SYMBOL, tradeSignal.PLATFORM, tradeSignal.TRADE_TIMEFRAME)
             
-            if(executeTradeRes != null) {
-                let tradeObj = {
-                    emailId: signal.EMAIL_ID,
-                    botSymbol: signal.BOT_SYMBOL,
-                    botExchange: signal.BOT_EXCHANGE,
-                    botTimeframe: signal.BOT_TIMEFRAME,
-                    botName: signal.BOT_NAME,
-                    endpointURL: signal.ENDPOINT_URL+'/api/v3/order',
-                    tradeSlippage: signal.TRADE_SLIPPAGE,
-                    tradeQuantity: signal.TRADE_QUANTITY,
-                    tradeAction: executeTradeRes.side,
-                    tickerPrice: executeTradeRes.fills[0].price,
-                    tradeConfirmationJSON: JSON.stringify(executeTradeRes),
-                    tradeStatus: 1,
-                    orderId: executeTradeRes.orderId,
-                    orderType: executeTradeRes.type
-                };
+            console.log('===> Signal Input Response:', signals);
 
-                console.log('###tradeObj###: ', tradeObj);
+            //Executing trades based on the received signal response
+            for (let signal of signals) {
 
-                const res = await tradeService.addTradeData(tradeObj);
-                console.log('### final response ===> : ', res);
-            }
+                const limitOrderRes = await tradeService.executeLimitTrade(signal.API_KEY, signal.API_SECRET, signal.ENDPOINT_URL, signal.BOT_SYMBOL, signal.TRADE_QUANTITY, signal.TRADE_ACTION)
+                
+                console.log('limitOrderRes: ', limitOrderRes)
+                 
+                if(limitOrderRes != null) {
+                    let tradeObj = {
+                        emailId: signal.EMAIL_ID,
+                        botSymbol: signal.BOT_SYMBOL,
+                        botExchange: signal.BOT_EXCHANGE,
+                        botTimeframe: signal.BOT_TIMEFRAME,
+                        botName: signal.BOT_NAME,
+                        apiKey: signal.API_KEY,
+                        apiSecret: signal.API_SECRET,
+                        endpointURL: signal.ENDPOINT_URL+'/api/v3/order',
+                        tradeSlippage: signal.TRADE_SLIPPAGE,
+                        tradeQuantity: signal.TRADE_QUANTITY,
+                        tradeAction: limitOrderRes.side,
+                        tickerPrice: limitOrderRes?.fills[0]?.price,
+                        tradeConfirmationJSON: JSON.stringify(limitOrderRes),
+                        tradeStatus: 1,
+                        orderId: limitOrderRes.orderId,
+                        orderType: limitOrderRes.type
+                    };
+
+                    console.log('### tradeObj ###: ', tradeObj);
+
+                    const res = await tradeService.addTradeData(tradeObj);
+                    console.log('### trade confirmation insert response ===> : ', res);
+                }
+            };
         };
-
-        //########To Do: Sending a success response after execute trade API call and insert table operation
 
         res.send({
             statusCode: res.statusCode,
             statusMessage: 'success',
-            message: 'Successfully processed the signals',
+            message: 'Successfully processed the signals with limit orders',
         });
 
     } catch (error) {
-        console.error('Error fetching trade history:', error.response ? error.response.data : error.message);
+        console.error('Error processing trade signals:', error.response ? error.response.data : error.message);
         res.status(500).json({
             statusCode: res.statusCode,
             statusMessage: 'error',
-            message: `Error fetching trade history:', ${error.response ? error.response.data : error.message}`,
+            message: `Error processing trade signals:', ${error.response ? error.response.data : error.message}`,
+        });
+    }
+}
+
+exports.executeMarketOrder = async (req, res) => {
+    try {
+        console.log('===> Inside executeMarketOrder()');
+
+        const trades = await tradeService.getTradeConfirmationData();
+
+        if(!trades) {
+            res.status(500).json({
+                statusCode: res.statusCode,
+                statusMessage: 'error',
+                message: `Error fetching trade confirmation data.`,
+            });
+        }
+
+        console.log('===> ## Trade Confirmation Data Response:', trades);
+
+        for (let trade of trades) {
+            //apiKey, apiSecret, endpointOrderUrl, symbol, quantity, side, orderId
+
+            console.log('===> trade.API_KEY:', trade.API_KEY);
+            console.log('===> trade.API_SECRET:', trade.API_SECRET);
+            console.log('===> trade.ENDPOINT_URL:', trade.ENDPOINT_URL);
+            console.log('===> trade.TRADE_SYMBOL:', trade.TRADE_SYMBOL);
+            console.log('===> trade.TRADE_QUANTITY:', trade.TRADE_QUANTITY);
+            console.log('===> trade.TRADE_ACTION:', trade.TRADE_ACTION);
+
+            const tradeRes = await tradeService.checkAndExecuteMarketOrder(trade.API_KEY, trade.API_SECRET, trade.ENDPOINT_URL, trade.TRADE_SYMBOL, trade.TRADE_QUANTITY, trade.TRADE_ACTION, trade.ORDER_ID)
+            
+            console.log('tradeRes: ', tradeRes)
+
+            if(tradeRes != null) {
+                let tradeObj = {
+                    tradeId: trade.TRADE_ID,
+                    tickerPrice: tradeRes?.fills ? tradeRes?.fills[0]?.price : tradeRes.price,
+                    tradeConfirmationJSON: JSON.stringify(tradeRes),
+                    tradeStatus: 0,
+                    orderId: tradeRes.orderId,
+                    orderType: tradeRes.type
+                };
+
+                console.log('###tradeObj###: ', tradeObj);
+
+                const res = await tradeService.updateTradeConfirmation(tradeObj);
+                console.log('### trade confirmation update response ===> : ', res);
+            }
+        };
+
+        res.send({
+            statusCode: res.statusCode,
+            statusMessage: 'success',
+            message: 'Successfully processed pending trades with market order',
+        });
+
+    } catch (error) {
+        console.error('Error processing pending trades:', error.response ? error.response.data : error.message);
+        res.status(500).json({
+            statusCode: res.statusCode,
+            statusMessage: 'error',
+            message: `Error processing pending trades:', ${error.response ? error.response.data : error.message}`,
         });
     }
 }
--- trades/routes/trade.routes.js
+++ trades/routes/trade.routes.js
@@ -5,5 +5,6 @@ const tradesController = require("../controllers/trade.controller.js");
 
 // Routes
 router.post("/signalinput", tradesController.signalInput);
+router.post("/executemarketorder", tradesController.executeMarketOrder);
 
 module.exports = router;
